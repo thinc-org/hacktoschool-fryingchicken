3ec5ef91df19953a66a319b275080ff0989e55d5
nac <chotpisit080916c@gmail.com>
2023-01-10 00:56:47 +0700

    feat(backend): setup prisma for nestjs

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(process.env.DATABASE_URL), PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
