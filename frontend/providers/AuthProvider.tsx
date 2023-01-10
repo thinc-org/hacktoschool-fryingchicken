21dc1bc068fb7a1e05ccfd14a9765a43401618f0
Kamanun Maneesri <jomlovejen7956@gmail.com>
2023-01-10 20:42:11 +0700

    feat: add login/register page

---
+++ frontend/providers/AuthProvider.tsx
@@ -0,0 +1,83 @@
+import { createContext, ReactNode, useContext, useRef, useState } from "react";
+import { AxiosError } from "axios";
+import { api } from "../utils/axios";
+import toast from "react-hot-toast";
+import { ErrorDto } from "../types/dto";
+
+interface IAuthContext {
+  isLoggedIn: boolean;
+  email: string | null;
+  login: (username: string, password: string, role: string) => Promise<void>;
+  logout: () => void;
+}
+
+interface AuthProviderProps {
+  children: ReactNode;
+}
+
+const AuthContext = createContext<IAuthContext | null>(null);
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+  return context;
+};
+
+const token = localStorage.getItem("token");
+const localEmail = localStorage.getItem("email");
+
+const AuthProvider = (props: AuthProviderProps) => {
+  const { children } = props;
+  const [isLoggedIn, setLoggedIn] = useState(!!token);
+  const [email, setEmail] = useState(localEmail);
+
+  const login = async (email: string, password: string, role: string) => {
+    email = email.trim();
+
+    try {
+      const res = await api.post("/auth/login", {
+        email,
+        password,
+      });
+
+      localStorage.setItem("token", res.data.Authorization);
+      localStorage.setItem("email", email);
+      localStorage.setItem("role", role);
+      setEmail(email);
+      setLoggedIn(true);
+    } catch (err) {
+      if (err instanceof AxiosError) {
+        const { response } = err as AxiosError<ErrorDto>;
+        const message = response?.data.message;
+        if (message) throw new Error(message);
+      }
+      throw new Error("Unknown error");
+    }
+  };
+
+  const logout = () => {
+    toast.success("Log out successfully");
+    localStorage.removeItem("token");
+    localStorage.removeItem("email");
+    localStorage.removeItem("role");
+    setEmail(null);
+    setLoggedIn(false);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn,
+        email,
+        login,
+        logout,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
---
+++ frontend/utils/axios.ts
@@ -0,0 +1,34 @@
+import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from "axios";
+
+// const API_URL = process.env.API_URL;
+// const API_URL = "https://cugetrekt-backend-lgnud3ncza-as.a.run.app"
+const API_URL = "http://localhost:3001";
+
+export const api = axios.create({
+  baseURL: API_URL,
+  timeout: 10000,
+});
+
+api.interceptors.request.use(
+  async (config: any) => {
+    const token = localStorage.getItem("token");
+    if (!token) return config;
+    return {
+      ...config,
+      headers: { ...config.headers, Authorization: `Bearer ${token}` },
+    };
+  },
+  (err: AxiosError) => Promise.reject(err)
+);
+
+api.interceptors.response.use(
+  (res: AxiosResponse) => res,
+  (err: AxiosError) => {
+    const status = err.response?.status;
+    if (status === 401) {
+      localStorage.removeItem("token");
+      window.location.href = "/login";
+    }
+    return Promise.reject(err);
+  }
+);
