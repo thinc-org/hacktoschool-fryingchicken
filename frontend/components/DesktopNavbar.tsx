fc0f1e4d4df5444025ab4cc8c8ce090882e53fc8
Joo <vijak13019@hotmail.com>
2023-01-10 19:04:59 +0700

    feat: remove dela font

--- frontend/components/DesktopNavbar.tsx
+++ frontend/components/DesktopNavbar.tsx
@@ -9,19 +9,19 @@ const DesktopNavbar = ({ links }: props) => {
   return (
     <header className="hide-md bg-white nav-container flex justify-between py-[2%] px-[4%] content-center sticky top-0">
       <div className="flex content-center">
-        <h1 className="font-bold tracking-[-0.05rem] text-2xl text-center">
+        <h1 className="font-black tracking-[-0.05rem] text-2xl text-center">
           Global Talk
         </h1>
         <div className="h-[25px] ver-line ml-10 self-center"></div>
-        <nav>
+        <nav className="self-center">
           {
-            <ul className="flex content-center">
+            <ul className="flex">
               {links.map((link, index) => {
                 return (
                   <a
                     key={index}
                     href={link.link}
-                    className="text-grey-dark hover:text-cyan-dark text-xl pl-10"
+                    className="text-grey-dark hover:scale-[1.05] hover:text-black text-xl pl-10 self-center"
                   >
                     {link.name}
                   </a>
@@ -31,7 +31,7 @@ const DesktopNavbar = ({ links }: props) => {
           }
         </nav>
       </div>
-      <h2 className="text-[#757575] text-xl">Sign In</h2>
+      <h2 className="text-[#757575] text-xl font-montserrat">Sign In</h2>
     </header>
   );
 };
