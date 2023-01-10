8bf22f8c817500d4ee093cb0db791b5b7f38facc
Joo <vijak13019@hotmail.com>
2023-01-10 13:51:20 +0700

    ci(tailwind): add colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -10,6 +10,21 @@ module.exports = {
       fontFamily: {
         'dela-Gothic-One': ['Dela Gothic One', 'cursive'],
         'montserrat': ['Montserrat', 'sans-serif']
+      },
+      colors: {
+        'cyan-dark': '#2B788B',
+        'cyan-light': '#C3DCE3',
+        'cyan-medium': '#5996A5',
+        'pink-dark': '#945069',
+        'pink-light': '#F2D4DC',
+        'grey-light': '#F6F5F4',
+        'grey-medium': '#E0E0E0',
+        'grey-icon': '#BABABA',
+        'grey-dark': '#757575',
+        'green-medium': '#639B6D',
+        'pink-medium': '#A15993',
+        'red-medium': '#A95151',
+        'yellow-medium': '#C4A24C'
       }
     },
   },
