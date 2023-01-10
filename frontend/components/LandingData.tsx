edb180cffbee3f7e7a83e05b191fa57bb451d222
Joo <vijak13019@hotmail.com>
2023-01-10 20:48:35 +0700

    feat: add responsive landing page

--- frontend/components/LandingData.tsx
+++ frontend/components/LandingData.tsx
@@ -5,11 +5,13 @@ const LandingData = ({ num, title }: { num: number; title: string }) => {
   return (
     <div className="flex flex-col items-center">
       <div className="flex items-center">
-        <AiFillThunderbolt className="text-4xl text-cyan-dark" />
-        <span className="font-bold text-5xl">{num}</span>
-        <BiPlusMedical className="text-3xl text-cyan-dark" />
+        <AiFillThunderbolt className="text-xl sm:text-4xl text-cyan-dark" />
+        <span className="text-2xl font-bold sm:text-5xl">{num}</span>
+        <BiPlusMedical className="text-base sm:text-3xl text-cyan-dark" />
       </div>
-      <span className="mt-1 text-lg font-semibold text-grey-dark">{title}</span>
+      <span className="text-sm mt-1 sm:text-lg font-semibold text-grey-dark">
+        {title}
+      </span>
     </div>
   );
 };
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -8,12 +8,12 @@ import LandingData from '../components/LandingData';
 const Home: NextPage = () => {
   return (
     <>
-      <main className="px-[8%] py-[3%] flex justify-between overflow-x-hidden">
+      <main className="px-[8%] my-[3%] flex flex-col justify-between overflow-x-hidden md:flex-row text-center md:text-left">
         <div className="">
-          <h2 className="text-cyan-dark font-bold font-montserrat text-lg uppercase tracking-[0.15rem]">
+          <h2 className="text-cyan-dark font-bold text-sm sm:text-lg uppercase tracking-[0.15rem]">
             E-COURSE PLATFORM
           </h2>
-          <h1 className="py-[3%] font-montserrat text-[4rem] tracking-[-0.05rem] leading-[4rem] font-extrabold">
+          <h1 className="py-[3%] text-3xl sm:text-[4rem] tracking-[-0.05rem] sm:leading-[4rem] font-extrabold">
             Learning and
             <br />
             teaching online,
@@ -21,10 +21,8 @@ const Home: NextPage = () => {
             made easy.
             <br />
           </h1>
-          <h2 className="text-grey-dark text-lg font-semibold pt-2">
-            Practice your English and learn new things
-            <br />
-            with the platform.
+          <h2 className="text-grey-dark text-sm sm:text-lg font-semibold pt-2">
+            Practice your English and learn new things with the platform.
           </h2>
           <a
             className="my-8 py-3 inline-block bg-cyan-light rounded-full px-5 font-bold text-cyan-dark hover:bg-cyan-dark hover:text-white transition-all duration-300"
@@ -32,25 +30,25 @@ const Home: NextPage = () => {
           >
             About Platform
           </a>
-          <div className="flex content-center justify-between w-[80%] items-center">
+          <div className="flex content-center justify-between w-[80%] items-center mx-auto">
             <LandingData num={600} title={'Popular words'} />
             <div className="h-[70px] ver-line"></div>
             <LandingData num={700} title={'Hours of content'} />
           </div>
         </div>
-        <div className="relative right-[-4rem]">
+        <div className="relative right-[-4rem] w-[100%] md:w-[50%] lg:pt-[1%] h-[80vh]">
           <Image
-            className="md:absolute md:z-[-1] md:max-w-[12rem] md:left-[-700px] md:top-[125px]"
+            className="hidden lg:block lg:absolute lg:z-[-1] lg:max-w-[30%] lg:left-[-10%] lg:top-[25%]"
             src={bgTree}
             alt="background tree"
           />
           <Image
-            className="md:absolute md:z-[1] md:right-[-150px] md:top-0 md:max-w-lg"
+            className="hidden lg:block lg:absolute lg:z-[1] lg:right-[-150px] lg:max-w-[70%] lg:bottom-[5%]"
             src={frontTree}
             alt="front tree"
           />
           <Image
-            className="md:absolute md:right-[100px] md:pt-4 md:max-w-[34rem]"
+            className="max-w-[80%] top-[5%] left-[-5%] sm:top-[30%] absolute sm:max-w-[90%] lg:top-[10%]"
             src={lyingBoy}
             alt="lying boy"
           />
