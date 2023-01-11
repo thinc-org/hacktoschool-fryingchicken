36867dda760c692c6a57f4c2fb7d4bda50ce20ab
nac <chotpisit080916c@gmail.com>
2023-01-11 18:54:29 +0700

    feat(backend): add auth using local

---
+++ backend/src/auth/auth.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
+
+@Injectable()
+export class AuthService {
+  constructor(private userService: UsersService) {}
+  async validateUser(username: string, password: string): Promise<any> {
+    const user = await this.userService.findOne(username);
+    // Add Hash Encryption Later
+    if (user && user.password === password) {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { password, ...result } = user;
+      return result;
+    }
+    return null;
+  }
+}
