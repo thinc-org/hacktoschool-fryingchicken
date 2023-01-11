b55cf7b4c1fc12c437240946de00590e1e7cfa8b
Kamanun Maneesri <jomlovejen7956@gmail.com>
2023-01-11 20:46:20 +0700

    fix(mycourses): temporary change mycourses code to all-courses

--- frontend/pages/mycourses.tsx
+++ frontend/pages/mycourses.tsx
@@ -1,18 +1,61 @@
 import Head from 'next/head';
 import SingleCourses from '../components/SingleCourses';
-import axios from 'axios';
+import { api } from '../utils/axios';
 import { useEffect, useState } from 'react';
+import { CourseDetailDto } from '../models/Dto';
 
-interface CourseProp {
-  courseName: string;
-}
 const MyCourses = () => {
-  const [data, setData] = useState<CourseProp[]>([]);
+  const [data, setData] = useState<CourseDetailDto[]>([]);
 
   useEffect(() => {
     const getData = async () => {
-      const res = await axios.get('http://localhost:3001/course');
-      const prop = await res.data;
+      // Todo: remove comment
+      // const res = await api.get('courses');
+      // const prop = await res.data;
+      const prop = [
+        {
+          id: 1,
+          name: 'General Philosophy',
+          instructorId: 313,
+          instructorName: 'Nac Nacho',
+          description: 'You will learn the meaning of life from this course',
+        },
+        {
+          id: 2,
+          name: 'General Philosophy',
+          instructorId: 313,
+          instructorName: 'Nac Nacho',
+          description: 'You will learn the meaning of life from this course',
+        },
+        {
+          id: 3,
+          name: 'General Philosophy',
+          instructorId: 313,
+          instructorName: 'Nac Nacho',
+          description: 'You will learn the meaning of life from this course',
+        },
+        {
+          id: 4,
+          name: 'General Philosophy',
+          instructorId: 313,
+          instructorName: 'Nac Nacho',
+          description: 'You will learn the meaning of life from this course',
+        },
+        {
+          id: 5,
+          name: 'General Philosophy',
+          instructorId: 313,
+          instructorName: 'Nac Nacho',
+          description: 'You will learn the meaning of life from this course',
+        },
+        {
+          id: 6,
+          name: 'General Philosophy',
+          instructorId: 313,
+          instructorName: 'Nac Nacho',
+          description: 'You will learn the meaning of life from this course',
+        },
+      ];
       setData(prop);
     };
     getData();
@@ -21,16 +64,16 @@ const MyCourses = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
-        <title>My Courses</title>
+        <title>All Courses</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">My Courses</h1>
+        <h1 className="text-6xl font-bold">All Courses</h1>
 
         <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          {data.map((prop: CourseProp) => (
-            <SingleCourses docsName={prop.courseName} />
+          {data.map((prop: CourseDetailDto) => (
+            <SingleCourses data={prop} key={prop.id} />
           ))}
         </div>
       </main>
