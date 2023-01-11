7eb1281880ba00d948f7ff137eb1fe86f04c49d2
Chanatip Pattanapen <lovels2906@gmail.com>
2023-01-11 21:31:03 +0700

    finish-api-mycourse-page

--- frontend/pages/mycourse.tsx
+++ frontend/pages/mycourse.tsx
@@ -5,27 +5,81 @@ export default function mycourse_instructor() {
   const [courses, setCourse] = useState([]);
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
-  const fetshCourse = async () => {
+  // const courses = [
+  //   {
+  //     id: '1',
+  //     name: 'sdfghjk',
+  //     instructorname: 'asdfghjk',
+  //     description: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+  //   },
+  //   {
+  //     id: '1',
+  //     name: 'sdfghjk',
+  //     instructorname: 'asdfghjk',
+  //     description: 'fdjoidjsfdjj;bfdnjfs',
+  //   },
+  //   {
+  //     id: '1',
+  //     name: 'sdfghjk',
+  //     instructorname: 'asdfghjk',
+  //     description: 'fdjoidjsfdjj;bfdnjfs',
+  //   },
+  // ];
+  const fetchCourse = async () => {
     const response = await fetch('/tempAPIroute');
     const data = await response.json();
     setCourse(data);
   };
   useEffect(() => {
-    fetshCourse();
+    fetchCourse();
   });
+
+  const handleSubmit = async () => {
+    const response = await fetch('/temp', {
+      method: 'POST',
+      body: JSON.stringify({
+        name: { title },
+        description: { description },
+      }),
+    });
+    const resJson = await response.json();
+    if (response.status === 200) {
+      setTitle('');
+      setDescription('');
+    }
+    //should add error handle
+  };
+  let role = 'student';
   return (
     <div className="container mx-auto">
       {courses.map((course) => {
-        return <div>{/* SHOW COURSE */}</div>;
+        return (
+          <div
+            className="border-2 w-3/4 mx-auto my-3 rounded p-3"
+            key={course.id}
+          >
+            <h1 className="text-2xl font-bold">{course.name}</h1>
+            {role == 'student' ? (
+              <p>
+                {course.instructorname}
+                <br />
+                {course.description}
+              </p>
+            ) : (
+              <p>{course.description}</p>
+            )}
+          </div>
+        );
       })}
-      <button
-        className="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-        type="button"
-        onClick={() => setShowModal(true)}
-      >
-        Create a new course
-      </button>
-
+      {role == 'instructure' ? (
+        <button
+          className="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+          type="button"
+          onClick={() => setShowModal(true)}
+        >
+          Create a new course
+        </button>
+      ) : null}
       {showModal ? (
         <>
           <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
@@ -57,9 +111,11 @@ export default function mycourse_instructor() {
                       <input
                         // id="about"
                         // name="about"
+                        type="text"
                         className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
                         placeholder="Course name"
                         value={title}
+                        onChange={(e) => setTitle(e.target.value)}
                       />
                     </div>
                   </div>
@@ -79,6 +135,7 @@ export default function mycourse_instructor() {
                         placeholder="What is this course about?"
                         // defaultValue={''}
                         value={description}
+                        onChange={(e) => setDescription(e.target.value)}
                       />
                     </div>
                   </div>
@@ -92,11 +149,14 @@ export default function mycourse_instructor() {
                   >
                     Cancel
                   </button>
+
                   <button
                     className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                     type="button"
-                    onClick={() => setShowModal(false)}
-                    // >> post fn
+                    onClick={() => {
+                      handleSubmit();
+                      setShowModal(false);
+                    }}
                   >
                     Create course
                   </button>
