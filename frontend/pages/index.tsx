0f794111e689675f1fd74ea3fd3d14626d21a162
Joo <vijak13019@hotmail.com>
2023-01-11 17:42:02 +0700

    feat: add second component of landing page

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,12 +1,13 @@
 import type { NextPage } from 'next';
 import FirstComponent from '../components/LandingPage/FirstComponent';
+import SecondComponent from '../components/LandingPage/SecondComponent';
 
 const Home: NextPage = () => {
   return (
     <>
       <main>
         <FirstComponent />
-        <FirstComponent />
+        <SecondComponent />
       </main>
     </>
   );
