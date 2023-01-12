ecdc8dc62d4b1c057e0804130b4d039223fe2aad
Joo <vijak13019@hotmail.com>
2023-01-12 15:15:36 +0700

    fix(Layout): mycourse link now works correctly

--- frontend/components/Layout.tsx
+++ frontend/components/Layout.tsx
@@ -8,7 +8,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
   const links = [{ name: 'Courses', link: '/courses' }];
 
   if (isLoggedIn) {
-    links.push({ name: 'My Courses', link: 'mycourse' });
+    links.push({ name: 'My Courses', link: '/mycourse' });
   }
 
   return (
