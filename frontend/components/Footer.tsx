1087bb7f15ef7a342a2316e3e3f02e043bfe4b11
Kamanun Maneesri <jomlovejen7956@gmail.com>
2023-01-12 21:32:34 +0700

    feat: add logo thinc and cleverse

--- frontend/components/Footer.tsx
+++ frontend/components/Footer.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 interface props {
   links: {
     name: string;
@@ -20,8 +22,26 @@ const Footer = ({ links }: props) => {
       <hr className="w-[90%] h-[0.5px] mx-auto my-4 bg-grey-dark border-0 rounded"></hr>
       <div className="flex flex-col items-center">
         <section className="pb-[3%] lg:pb-[1%] flex justify-center w-[100vw]">
-          <span className="mx-[2%]">Cleverse</span>
-          <span className="mx-[2%]">Thinc</span>
+          <span className="mx-[2%]">
+            <Image
+              className="mx-auto"
+              src="/../public/Cleverse.jpg"
+              alt=""
+              width="40"
+              height="40"
+            />
+            Cleverse
+          </span>
+          <span className="mx-[2%]">
+            <Image
+              className="mx-auto"
+              src="/../public/Thinc.jpg"
+              alt=""
+              width="40"
+              height="40"
+            />
+            Thinc.
+          </span>
         </section>
         <span className="text-[0.6rem] sm:text-xs">
           ©2021 Thinc. x Cleverse. Project for <u>hack to school</u>
