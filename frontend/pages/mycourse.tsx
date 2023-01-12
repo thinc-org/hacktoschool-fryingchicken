b156ecbf27cbbc036e4d8bc5ca83dc2a837af787
Joo <vijak13019@hotmail.com>
2023-01-12 07:05:07 +0700

    refactor(create course modal): migrate codes to its own file
    Also change ternary operators to &&

--- frontend/pages/mycourse.tsx
+++ frontend/pages/mycourse.tsx
@@ -5,6 +5,7 @@ import { api } from '../utils/axios';
 import Link from 'next/link';
 import SingleCourses from '../components/SingleCourses';
 import Head from 'next/head';
+import CreateCourseModal from '../components/CreateCourseModal';
 
 export default function mycourse_instructor() {
   const { username, role } = useAuth();
@@ -69,6 +70,7 @@ export default function mycourse_instructor() {
       courseId,
       username,
     });
+    await getEnrolls();
   };
 
   if (index < enrolls.length) {
@@ -109,7 +111,7 @@ export default function mycourse_instructor() {
             );
           })}
 
-          {role == 'instructor' ? (
+          {role == 'instructor' && (
             <div className="flex justify-end w-3/4 mx-auto">
               <button
                 className="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
@@ -119,95 +121,18 @@ export default function mycourse_instructor() {
                 Create a new course
               </button>
             </div>
-          ) : null}
+          )}
 
-          {showModal ? (
-            <>
-              <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-                <div className="relative w-auto my-6 mx-auto max-w-3xl">
-                  {/*content*/}
-                  <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                    {/*header*/}{' '}
-                    <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                      <h3 className="text-3xl font-semibold">Course detail</h3>
-                      <button
-                        className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                        onClick={() => setShowModal(false)}
-                      >
-                        <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                          ×
-                        </span>
-                      </button>
-                    </div>
-                    {/*body*/}
-                    <div className="relative p-6 flex-auto">
-                      <div>
-                        <label
-                          htmlFor="about"
-                          className="block text-sm font-medium text-gray-700"
-                        >
-                          Title
-                        </label>
-                        <div className="mt-1">
-                          <input
-                            // id="about"
-                            // name="about"
-                            type="text"
-                            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                            placeholder="Course name"
-                            value={title}
-                            onChange={(e) => setTitle(e.target.value)}
-                          />
-                        </div>
-                      </div>
-                      <div>
-                        <label
-                          htmlFor="about"
-                          className="block text-sm font-medium text-gray-700"
-                        >
-                          Description
-                        </label>
-                        <div className="mt-1">
-                          <textarea
-                            // id="about"
-                            // name="about"
-                            rows={3}
-                            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                            placeholder="What is this course about?"
-                            // defaultValue={''}
-                            value={description}
-                            onChange={(e) => setDescription(e.target.value)}
-                          />
-                        </div>
-                      </div>
-                    </div>
-                    {/*footer*/}
-                    <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
-                      <button
-                        className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                        type="button"
-                        onClick={() => setShowModal(false)}
-                      >
-                        Cancel
-                      </button>
-
-                      <button
-                        className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                        type="button"
-                        onClick={() => {
-                          handleSubmit();
-                          setShowModal(false);
-                        }}
-                      >
-                        Create course
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
-            </>
-          ) : null}
+          {showModal && (
+            <CreateCourseModal
+              setShowModal={setShowModal}
+              setTitle={setTitle}
+              title={title}
+              setDescription={setDescription}
+              description={description}
+              handleSubmit={handleSubmit}
+            />
+          )}
         </div>
       </main>
     </div>
