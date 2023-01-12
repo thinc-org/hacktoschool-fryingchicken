9d7c200c832b546e9c461111973b114384c84192
nac <chotpisit080916c@gmail.com>
2023-01-12 20:31:09 +0700

    style(backend): remove unused comment

--- backend/src/utils/bcrypt.ts
+++ backend/src/utils/bcrypt.ts
@@ -1,9 +1,7 @@
 import * as bcrypt from 'bcrypt';
 
 export async function encodePassword(rawPassword: string): Promise<string> {
-  //   const SALT = '10';
   const SALT = bcrypt.genSaltSync();
-  console.log(SALT);
   return await bcrypt.hash(rawPassword, SALT);
 }
 
