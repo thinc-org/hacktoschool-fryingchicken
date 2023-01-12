143a1c11c011c8cbbe8075eb6e3c9136efa7cc2f
nac <chotpisit080916c@gmail.com>
2023-01-12 20:39:01 +0700

    refactor(backend): reorgnize auth methods

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -7,9 +7,9 @@ import {
   Redirect,
 } from '@nestjs/common';
 import { AppService } from './app.service';
-import { LocalAuthGuard } from './auth/local-auth.guard';
+import { LocalAuthGuard } from './auth/local/local-auth.guard';
 import { AuthService } from './auth/auth.service';
-import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { JwtAuthGuard } from './auth/jwt/jwt-auth.guard';
 
 @Controller()
 export class AppController {
