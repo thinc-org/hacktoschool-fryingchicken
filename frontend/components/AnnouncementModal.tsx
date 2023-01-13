84c354b21d5e2e2897bc018728fad9ff3a9a61fa
Chanatip Pattanapen <lovels2906@gmail.com>
2023-01-13 12:05:11 +0700

    fix: announcementModal

--- frontend/components/AnnouncementModal.tsx
+++ frontend/components/AnnouncementModal.tsx
@@ -1,20 +1,13 @@
+import { AnnouncementDetailDto } from '../models/Dto';
 interface props {
-  setShowAnnouncement: React.Dispatch<React.SetStateAction<boolean>>;
-  courseName: string;
-  title: string;
-  description: string;
-  createdDate: Date;
-  readList: string[];
+  setAnDetail: React.Dispatch<
+    React.SetStateAction<AnnouncementDetailDto | undefined>
+  >;
+
+  data: AnnouncementDetailDto;
 }
 
-export default function AnnouncementModal({
-  setShowAnnouncement,
-  courseName,
-  title,
-  description,
-  createdDate,
-  readList,
-}: props) {
+export default function AnnouncementModal({ data, setAnDetail }: props) {
   return (
     <>
       <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
@@ -22,27 +15,21 @@ export default function AnnouncementModal({
           {/*content*/}
           <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
             {/*header*/}
-            <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-              <h3 className="text-3xl font-semibold">{title}</h3>
-              <button
-                className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                onClick={() => setShowAnnouncement(false)}
-              >
-                <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                  ×
-                </span>
-              </button>
+            <div className="flex items-start flex-col justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+              <h3 className="text-3xl font-semibold">{data.title}</h3>
             </div>
             {/*body*/}
             <div className="relative p-6 flex-auto">
-              <h4>{description}</h4>
+              <h4>{data.description}</h4>
             </div>
             {/*footer*/}
             <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
               <button
                 className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                 type="button"
-                onClick={() => setShowAnnouncement(false)}
+                onClick={() => {
+                  setAnDetail(undefined);
+                }}
               >
                 Close
               </button>
