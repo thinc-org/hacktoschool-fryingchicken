9401466387143f627af0fa1d5f560608ee9a46a7
Kamanun Maneesri <jomlovejen7956@gmail.com>
2023-01-13 15:32:55 +0700

    fix(backend): add roleGuard to create method for course and announement

--- backend/src/announcement/announcement.controller.ts
+++ backend/src/announcement/announcement.controller.ts
@@ -6,12 +6,17 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { AnnouncementService } from './announcement.service';
 import { CreateAnnouncementDto } from './dto/create-announcement.dto';
 import { UpdateAnnouncementDto } from './dto/update-announcement.dto';
 import { ApiTags, ApiCreatedResponse, ApiOkResponse } from '@nestjs/swagger';
 import { AnnouncementEntity } from './entities/announcement.entity';
+import { JwtAuthGuard } from 'src/auth/jwt/jwt-auth.guard';
+import { RolesGuard } from 'src/auth/roles/roles.guard';
+import { Role } from 'src/auth/roles/role.enum';
+import { Roles } from 'src/auth/roles/roles.decorator';
 
 @Controller('announcement')
 @ApiTags('announcement')
@@ -19,6 +24,8 @@ export class AnnouncementController {
   constructor(private readonly announcementService: AnnouncementService) {}
 
   @Post()
+  @Roles([Role.Instructor, Role.Admin])
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @ApiCreatedResponse({ type: AnnouncementEntity })
   create(@Body() createAnnouncementDto: CreateAnnouncementDto) {
     return this.announcementService.create(createAnnouncementDto);
